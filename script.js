00c120aa254b56aa2ca2f86126073010b8ff3471
K00298934: Michael Dee <K00298934@student.tus.ie>
2024-10-25 13:51:41 +0100

    JavaScript file

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById(fetchDataButton).addEventListener('click',);{
+    fetch(picsum.photos/400/300?random=$:Math.random(),)
+    .then(Response=>Response.json);
+//  use .then again to return the data to the data container
+// use .catch to handle errors with the api
+    Animation(dataContainer,screenLeft);
+};
+const fetchDataButton = Response.json;
